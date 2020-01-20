dd57b84a7a692be7d3a3494f220518630b776f50
潘迪海 <2711002357@qq.com>
2020-01-20 10:42:37 +0800

    chore(g):lerna

---
+++ packages/script/containers/153/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "../../../../packages/container";
+
+
+export default function App() {
+  return (
+    <div>
+      <Container authUrl={"authUrl"} />
+    </div>
+  );
+}
---
+++ packages/script/containers/153/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import App from "./App";
+import { render } from "../../../../packages/container";
+
+render(App, document.getElementById("container"));
