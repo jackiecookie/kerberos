987714956c17a568a5b1e65c6bd369ecf3fbb7c0
潘迪海 <2711002357@qq.com>
2020-01-21 16:28:48 +0800

    chore

---
+++ packages/kerberos-app/tests/index.spec.tsx
@@ -0,0 +1,33 @@
+jest.mock("kerberos-utils/appLifeCycle");
+jest.mock("kerberos-utils/handleAssets");
+jest.mock("react-dom");
+
+import { appRegister } from "../index";
+import { appRegister as appLifeCycleAppRegister } from "kerberos-utils/appLifeCycle";
+import { isInContainer } from "kerberos-utils/handleAssets";
+import ReactDOM from "react-dom";
+import React from "react";
+
+ReactDOM.render = jest.fn();
+
+function Noop() {
+  return <div>Noop</div>;
+}
+
+isInContainer.mockReturnValueOnce(false).mockReturnValueOnce(true);
+
+describe("test appRegister", () => {
+  test("render component in root provide when not in container", () => {
+    // not in container
+    let rootId = "rootId";
+    appRegister(Noop, rootId);
+    expect(ReactDOM.render).toBeCalledWith(Noop(),rootId)
+  });
+
+  test("register callback into lifeCycle when in container", () => {
+    // in container
+    appRegister(Noop);
+    expect(appLifeCycleAppRegister).toBeCalledTimes(1);
+    //appLifeCycleAppRegister.mock.calls
+  });
+});
